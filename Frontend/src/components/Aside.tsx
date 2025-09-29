709fe6b4afdb4fdb2ab08d240bf4ad3a1fa3ac78
Anshu Kumar <anshukr95086@gmail.com>
2025-09-30 02:14:38 +0530

    made the chatApp side bar and implemented filtering feature

--- Frontend/src/components/Aside.tsx
+++ Frontend/src/components/Aside.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Card, CardTitle, CardContent } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Search } from "lucide-react";
+import { Input } from './ui/input';
 
-const Aside = () => {
+const Aside = ({friends}: {friends: string[]}) => {
+  const [search, setSearch] = useState<string>("");
+    const [filteredFriends,  setFilteredFriends] = useState<string[]>(friends);
+  const searchFriends = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value);
+      
+      const filtered = friends.filter(friend => friend.toLowerCase().includes(e.target.value.toLowerCase()));
+      setFilteredFriends(filtered)
+    }
+
   return (
-    <div>Aside</div>
+    <>
+    <div className="mx-2 mt-4 relative">
+            <Search className="absolute left-2 top-1/2 -translate-y-1/2 text-muted-foreground" />
+            <Input
+              className="pl-10"
+              type="text"
+              placeholder="Search..."
+              value={search}
+              onChange={(e) => searchFriends(e)}
+            />
+          </div>
+          <div className="w-full flex flex-col gap-3 max-h-screen overflow-y-scroll mt-4">
+            {filteredFriends.map((friend, index) => (
+              <Card
+                className=" mx-2 h-14 flex justify-center cursor-pointer"
+                key={index}
+              >
+                <CardContent className="flex gap-3 items-center">
+                  <Avatar>
+                    <AvatarImage />
+                    <AvatarFallback>
+                      {friend.charAt(0).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <CardTitle>{friend}</CardTitle>
+                </CardContent>
+              </Card>
+            ))}
+      </div>
+      </>
   )
 }
 
--- Frontend/src/pages/Chat.tsx
+++ Frontend/src/pages/Chat.tsx
@@ -2,52 +2,84 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { useSocket } from "@/context/SocketProvider";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import React from "react";
 
+import Aside from "@/components/Aside";
 
 const Chat = () => {
-  const [message, setMessage] = useState<string>("")
-  const [messages, setMessages] = useState<string[]>([])
-  const {sendMessage} = useSocket()
+  const [message, setMessage] = useState<string>("");
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const { sendMessage } = useSocket();
+  const friends = [
+    "alex123",
+    "john_doe",
+    "mary99",
+    "coder_ash",
+    "sophia_k",
+    "dev_master",
+    "anuj45",
+    "tech_girl",
+    "rahul007",
+    "neha_dev",
+    "bjhdvsdf",
+  ];
+
+ 
+
 
   const handleSend = (e: React.FormEvent) => {
-    e.preventDefault()
-    sendMessage(message)
-    setMessages([...messages, message])
-    setMessage("")
-  }
+    e.preventDefault();
+    sendMessage(message);
+    setMessages([...messages, message]);
+    setMessage("");
+  };
+
   return (
     <>
-      <div className="w-full">
-        <section>
-          <h1 className="text-3xl text-center w-full font-bold underline">
-            Chat Page
-          </h1>
-        </section>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel className="max-w-1/2 min-w-1/4">
+          <Aside friends={friends}/>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>
+          {" "}
+          <div className="w-full">
+            {/* messages */}
+            <section className="min-h-[90vh]">
+              {messages.map((msg, index) => (
+                <div key={index}>{msg}</div>
+              ))}
+            </section>
 
-        {/* messages */}
-        <section className="min-h-[85vh]">
-         { messages.map((msg, index) => (
-           <div key={index}>{msg}</div>
-          ))}
-        </section>
-
-        <section className="w-full">
-          <form onSubmit={handleSend} className="flex gap-4 justify-center">
-            <Input
-              type="text"
-              placeholder="Type a message"
-              className="w-[80vw]"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            <Button onClick={handleSend} type="submit" className="w-15 font-medium">
-              Send
-            </Button>
-          </form>
-        </section>
-      </div>
+            <section className="w-full mx-8">
+              <form onSubmit={handleSend} className="flex gap-4 justify-center">
+                <Input
+                  type="text"
+                  placeholder="Type a message"
+                  className="w-[80vw]"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+                <Button
+                  onClick={handleSend}
+                  type="submit"
+                  className="w-15 font-medium"
+                >
+                  Send
+                </Button>
+              </form>
+            </section>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </>
   );
-}
+};
 
-export default Chat
+export default Chat;
